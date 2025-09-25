5b37af6e110264b915c72f1e7505e1ae646477b5
Jonatan Elias <jonatanceballosc3@example.com>
2025-09-25 17:57:58 -0500

    lista

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,72 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         menu();
     }
     public static void menu(){
         Scanner teclado=new Scanner(System.in);
         CuentaBanco b1 =new CuentaBanco();
+        List <CuentaBanco> lista=new ArrayList();
         boolean bandera=true;
-        int opcion;
+        int opcion,numeroCuenta,cedula;
         double dineroIngresado,dineroRetirado;
         do {
             System.out.println("Ingrese una opcion");
-            System.out.println("1.1Regristrar cuenta");
-            System.out.println("2.Consultar saldo");
-            System.out.println("3.Ingresar dinero");
-            System.out.println("4.Retirar dinero");
-            System.out.println("5.Retiro rapido (20%)");
-            System.out.println("6.Salir");
+            System.out.println("1.Regristrar cuenta");
+            System.out.println("2.Mostrar Cuentas");
+            System.out.println("3.Eliminar cuenta");
+            System.out.println("4.Buscar cuenta");
+            System.out.println("5.Salir");
+
             opcion=teclado.nextInt();
             switch (opcion){
                 case 1:
                     System.out.println("Ingrese el numero de cuenta");
-                    b1.setNumeroCuenta(teclado.nextInt());
+                    numeroCuenta=teclado.nextInt();
                     System.out.println("Ingrese el numero de cedula");
-                    b1.setCedula(teclado.nextInt());
+                    cedula=teclado.nextInt();
                     System.out.println("Ingrese cuanta plata tiene");
-                    b1.setSaldoActual(teclado.nextInt());
+                    dineroIngresado=teclado.nextDouble();
+                    lista.add(new CuentaBanco(numeroCuenta,cedula,dineroIngresado));
                     break;
                 case 2:
-                    System.out.println("Su saldo actual es de: "+b1.consultarSaldo());
+                    for(CuentaBanco p:lista){
+                        System.out.println(p);
+                    }
                     break;
                 case 3:
-                    System.out.println("Ingrese el valor a ingresar");
-                    dineroIngresado=teclado.nextDouble();
-                    b1.ingresarDinero(dineroIngresado);
+                    System.out.println("Ingrese el numero de cuenta a eliminar");
+                    numeroCuenta=teclado.nextInt();
+                    for(CuentaBanco p:lista){
+                        if(p.getNumeroCuenta()==numeroCuenta){
+                            lista.remove(p);
+                        }
+                    }
                     break;
                 case 4:
-                    System.out.println("Ingrese el valor a retirar");
-                    dineroRetirado=teclado.nextDouble();
-                    b1.retirarDinero(dineroRetirado);
-
+                    System.out.println("Ingrese el numero de cuenta");
+                    numeroCuenta=teclado.nextInt();
+                    for(CuentaBanco p:lista){
+                        if(p.getNumeroCuenta()==numeroCuenta){
+                            submenu
+                        }
+                    }
+                    submenu(numeroCuenta);
                     break;
                 case 5:
-                    b1.extraccionRapida();
-                    break;
-                case 6:
                     bandera=false;
                     break;
                 default:
                     System.out.println("Opcion no valida");
             }
         }while (bandera);
+    }
+    public static void submenu(){
+
+
 
     }
 }
