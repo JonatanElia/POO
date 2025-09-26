7069e4a9a118c5b47a8b07b0f12b0694a933a734
Jonatan <jonatanceballosc3@gmail.com>
2025-09-26 00:15:17 -0500

    Submenu

--- src/Main.java
+++ src/Main.java
@@ -51,10 +51,9 @@ public class Main {
                     numeroCuenta=teclado.nextInt();
                     for(CuentaBanco p:lista){
                         if(p.getNumeroCuenta()==numeroCuenta){
-                            submenu
+                            submenu(p);
                         }
                     }
-                    submenu(numeroCuenta);
                     break;
                 case 5:
                     bandera=false;
@@ -64,9 +63,48 @@ public class Main {
             }
         }while (bandera);
     }
-    public static void submenu(){
+    public static void submenu(CuentaBanco b1){
+        Scanner teclado=new Scanner(System.in);
+        boolean bandera=true;
+        int opcion;
+        double dineroIngresado,dineroRetirado;
+        do {
+            System.out.println("Ingrese una opcion");
+            System.out.println("1.Informacion de cuenta");
+            System.out.println("2.Consultar saldo");
+            System.out.println("3.Ingresar dinero");
+            System.out.println("4.Retirar dinero");
+            System.out.println("5.Retiro rapido (20%)");
+            System.out.println("6.Salir");
+            opcion=teclado.nextInt();
+            switch (opcion){
+                case 1:
+                    System.out.println(b1);
+                    break;
+                case 2:
+                    System.out.println("Su saldo actual es de: "+b1.consultarSaldo());
+                    break;
+                case 3:
+                    System.out.println("Ingrese el valor a ingresar");
+                    dineroIngresado=teclado.nextDouble();
+                    b1.ingresarDinero(dineroIngresado);
+                    break;
+                case 4:
+                    System.out.println("Ingrese el valor a retirar");
+                    dineroRetirado=teclado.nextDouble();
+                    b1.retirarDinero(dineroRetirado);
 
-
+                    break;
+                case 5:
+                    b1.extraccionRapida();
+                    break;
+                case 6:
+                    bandera=false;
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+            }
+        }while (bandera);
 
     }
 }
